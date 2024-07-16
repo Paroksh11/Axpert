12030a5d0b42cbaf7ca8fe9688afb6a43cecfb3f
Paroksh11 <153581440+Paroksh11@users.noreply.github.com>
2024-07-16 19:25:07 +0530

    a

---
+++ AxpertReleases/OfficialReleases/Patches/Version 11.3/Patch1/AxpertDeveloper/AxpertStructures/Postgre/After_Import_Script.sql
@@ -0,0 +1,13 @@
+<<
+update coretstructhdr set pattern=patternddl where pattern='patternddl';
+>>
+
+
+<<
+update coretstructhdr set masking ='No Mask' where (masking is null or length(masking)<2);
+>>
+
+
+<<
+update dwb_iviewmain set hideprojname ='F',hidereportdate ='F';
+>>
